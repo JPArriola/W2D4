2dbd1305b839db3421d357598ccff0138ace7b96
JPArriola <joshuapatrickarriola@gmail.com>
2018-11-01 16:27:54 -0700

    Add second_anagram solution

--- Anagrams/anagrams.rb
+++ Anagrams/anagrams.rb
@@ -6,3 +6,16 @@ def first_anagram?(string1, string2)
   
   false
 end
+
+def second_anagram?(string1, string2)
+  string2_arr = string2.split("")
+  
+  string1.chars.each do |el|
+    
+    idx_for_string2 = string2_arr.find_index(el)
+    return false if idx_for_string2.nil?
+    string2_arr.delete(string2_arr[idx_for_string2])
+  end
+  string2_arr.empty?
+    
+end
