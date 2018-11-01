401367b41b953be7cecd5a9db800cffb96ba0ae6
JPArriola <joshuapatrickarriola@gmail.com>
2018-11-01 16:28:50 -0700

    Add fourth_anagram solution

--- Anagrams/anagrams.rb
+++ Anagrams/anagrams.rb
@@ -23,3 +23,22 @@ end
 def third_anagram?(string1, string2)
   string1.chars.sort.join == string2.chars.sort.join
 end
+
+def fourth_anagram?(string1, string2)
+  hash1 = Hash.new(0)
+  hash2 = Hash.new(0)
+  
+  string1.chars.each do |char|
+    hash1[char] += 1
+  end
+  
+  string2.chars.each do |char|
+    hash2[char] += 1
+  end
+  
+  hash1.keys.each do |key|
+    return false if hash1[key] != hash2[key]
+  end  
+  
+  true
+end
