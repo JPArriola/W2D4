f293b56a1ddb9adfc76d2dda53b4d8a25c636260
JPArriola <joshuapatrickarriola@gmail.com>
2018-11-01 16:49:39 -0700

    Add brute force and okay_two_sum

---
+++ Two_Sum/two_sum.rb
@@ -0,0 +1,34 @@
+def bf_two_sum?(arr, target)
+  arr.each_with_index do |num, idx|
+    (idx+1...arr.length).each do |jdx|
+      return true if arr[idx] + arr[jdx] == target
+    end
+  end
+  false
+  
+end
+
+def okay_two_sum?(arr, target)
+  
+  
+  until arr.length <= 1
+    return true if (arr.first + arr.last) == target
+    
+    if arr.first + arr.last > target
+      arr.pop
+    else
+      arr.shift
+    end
+  end
+  false
+end
+  
+  
+
+
+
+
+
+# arr = [0, 1, 5, 7]
+# bf_two_sum?(arr, 6) # => should be true
+# bf_two_sum?(arr, 10)
